5866401e50e8d7ab608e4f40259bab90a0a479b8
dmitriy <dim4ik-super-klass@mail.ru>
2022-01-12 19:29:34 +0800

    nothing special

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,7 @@
 
 let block_changing_task;
 let block_task_list;
-
+let sample;
 task_list = [ 
 	{'is_complete':true, 'is_priority': false, 'discription': "dont do anyting"}, 
 	{'is_complete':true, 'is_priority': false, 'discription': "dont do anyting"},
@@ -14,9 +14,30 @@ task_list = [
 
 function on_create() {
 	block_task_list = document.querySelector(".block__display_tasks");
-	
+	sample = block_task_list.children[1];
+	block_task_list.removeChild(sample);
+	for(let i = 0; i < task_list.length; i++){
+		let new_task = document.createElement('li');
+		if(i % 2 == 0){
+			new_task.classList.add(sample.classList);
+		}
+		new_task.innerHTML = sample.innerHTML;
+		block_task_list.append(new_task);
+		//new_task.children[1].children[1].addEventListener('click', on_click_delete(new_task));
+		//new_task.children[1].children[0].addEventListener('click', on_click_save(new_task));
+	}
+	//on_start();
 }
 
+
+function on_click_delete(element){
+	element.parentNode.removeChild(element);
+
+}
+
+function on_click_save(element){
+	
+}
 
 function on_start() {
 
